41eb74c3487ce26f19f4e84b657c03aa14c24dba
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2024-08-31 00:26:19 +0530

    Delete 4. Inserting, Updating, and Deleting Data directory

--- 4. Inserting, Updating, and Deleting Data/Deleting raws.sql
+++
@@ -1,4 +0,0 @@
-delete from invoices
-where client_id in (select client_id
- from clients
-where name='Myworks')
--- 4. Inserting, Updating, and Deleting Data/Updating a Single Row.sql
+++
@@ -1,6 +0,0 @@
-update invoices 
--- set payment_total = default, payment_date = NULL
-set 
-    payment_total = invoice_total*0.5,
-    payment_date = due_date
-where invoice_id = 1
