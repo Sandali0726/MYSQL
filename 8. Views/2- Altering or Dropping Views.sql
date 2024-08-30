c36d8d6ffa91beb26af958a77f028a2f9b0511d7
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2024-08-31 00:27:08 +0530

    Delete 8. Views directory

--- 8. Views/2- Altering or Dropping Views.sql
+++
@@ -1,9 +0,0 @@
--- create or replace view sales_by_client as 
-create view sales_by_client as 
-select
-     c.client_id,
-     c.name,
-     sum(invoice_total) as total_sale
-from clients c 
-join invoices i using (client_id)
-group by client_id,name
